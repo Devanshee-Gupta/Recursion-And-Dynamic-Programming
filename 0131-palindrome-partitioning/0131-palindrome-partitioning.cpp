f2d0723eca12d66969f3599c513e3bba5ab4f660
Devanshee-Gupta <154060860+Devanshee-Gupta@users.noreply.github.com>
2024-01-02 02:46:15 +0530

    Time: 262 ms (9.72%), Space: 50 MB (76.62%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool check(int i,string &s){
+        if(i>=s.length()/2){
+            return 1;
+        }
+        
+        if(s[i]!=s[s.length()-i-1]) return 0;
+        
+        return check(i+1,s);
+    }
+    
+    void f(int ind,int n,string temp, string s, vector<string>&ds,vector<vector<string>>&ans){
+        
+        if(s.length()==0){
+            ans.push_back(ds);
+            return;
+        }
+        string copys=s;
+        for(int i=0;i<s.length();i++){
+            temp=s.substr(0,i+1);
+            s=s.substr(i+1,string::npos);
+            if(check(0,temp)){
+                ds.push_back(temp);
+                f(ind+1,n,temp,s,ds,ans);
+                ds.pop_back();
+            }
+            s=copys;
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>ds;
+        
+        f(0,s.length(),"",s,ds,ans);
+        
+        return ans;
+    }
+};
